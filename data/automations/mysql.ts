99c46773a6343da2b73e57c57978fca36adb36dc
noreplydev <contact.cristiansanchez@gmail.com>
2025-08-07 17:44:40 +0200

    add mysql extension

---
+++ data/automations/mysql.ts
@@ -0,0 +1,128 @@
+import { getAuth, getServiceToken } from "protonode";
+import { API, Protofy, getLogger } from "protobase";
+import APIContext from "app/bundles/context";
+import { Application } from 'express';
+import fs from 'fs'
+import path from "path";
+import mysql from 'mysql2'
+import { Db } from "@extensions/mysql/db";
+
+const root = path.join(process.cwd(), '..', '..')
+const logger = getLogger()
+const name = "mysql"
+const origin = 'automations'
+
+Protofy("type", "CustomAPI")
+
+async function dbCredentials(req, res, next) {
+    try {
+        // here we could also implement the db host, and more.
+        let dbName = (await API.get("/api/core/v1/keys/DB_NAME?token=" + getServiceToken())).data;
+        let dbUser = (await API.get("/api/core/v1/keys/DB_USER?token=" + getServiceToken())).data;
+        let dbPassword = (await API.get("/api/core/v1/keys/DB_PASSWORD?token=" + getServiceToken())).data;
+
+        if (!dbUser || !dbName || !dbPassword) {
+            return res.json({
+                is_error: true,
+                error: "DB secrets no provided. Required: DB_NAME, DB_USER, DB_PASSWORD",
+                data: null
+            })
+        }
+
+        req.DB_NAME = dbName
+        req.DB_USER = dbUser
+        req.DB_PASSWORD = dbPassword
+        next()
+    } catch (err) {
+        console.log(err)
+        res.json({
+            error: "-", // avoid giving to much information about the internal error
+            is_error: true,
+            data: null,
+        })
+    }
+}
+
+export default Protofy("code", async (app: Application, context) => {
+    app.get("/api/v1/mysql/tables", dbCredentials, async (req, res) => {
+        try {
+            const { DB_NAME, DB_USER, DB_PASSWORD } = req as any // avoid type errors caused by express typing
+            console.log({ DB_NAME, DB_USER, DB_PASSWORD })
+            const db = new Db(DB_USER?.value, DB_PASSWORD?.value, DB_NAME?.value)
+            const connected = await db.connect();
+            if (!connected) {
+                return res.json({
+                    error: "-", // avoid giving to much information about the internal error
+                    is_error: true,
+                    data: null,
+                })
+            }
+            let result = await db.getTables()
+            await db.close()
+            res.json({
+                is_error: false,
+                error: null,
+                data: result
+            })
+        } catch (err) {
+            console.log(err)
+            res.json({
+                error: "-", // avoid giving to much information about the internal error
+                is_error: true,
+                data: null,
+            })
+        }
+    })
+
+    app.get("/api/v1/mysql/table/:tableId", dbCredentials, async (req, res) => {
+        const { DB_NAME, DB_USER, DB_PASSWORD } = req as any // avoid type errors caused by express typing
+        const tableId = req.params.tableId
+        const page = Number(req.query.page) || 1
+
+        console.log({ DB_NAME, DB_USER, DB_PASSWORD })
+        const db = new Db(DB_USER?.value, DB_PASSWORD?.value, DB_NAME?.value)
+        const connected = await db.connect();
+        if (!connected) {
+            return res.json({
+                error: "-", // avoid giving to much information about the internal error
+                is_error: true,
+                data: null,
+            })
+        }
+        let result = await db.getTableItems(tableId, page)
+        res.json({
+            is_error: false,
+            error: null,
+            data: result
+        })
+        await db.close()
+        return
+    })
+
+
+    app.get("/api/v1/mysql/table/:tableId/:field/:fieldValue", dbCredentials, async (req, res) => {
+        const { DB_NAME, DB_USER, DB_PASSWORD } = req as any // avoid type errors caused by express typing
+        const { tableId, field, fieldValue } = req.params
+        const page = Number(req.query.page) || 1
+
+        console.log({ DB_NAME, DB_USER, DB_PASSWORD })
+        const db = new Db(DB_USER?.value, DB_PASSWORD?.value, DB_NAME?.value)
+        const connected = await db.connect();
+        if (!connected) {
+            return res.json({
+                error: "-", // avoid giving to much information about the internal error
+                is_error: true,
+                data: null,
+            })
+        }
+        let result = await db.getItem(tableId, field, fieldValue)
+        res.json({
+            is_error: false,
+            error: null,
+            data: result
+        })
+        await db.close()
+        return
+    })
+})
+
---
+++ extensions/mysql/db.ts
@@ -0,0 +1,150 @@
+import mysql from 'mysql2'
+
+export class Db {
+    connection: any
+
+    constructor(dbUser: string, dbPassword: string, dbName: string) {
+        let host = "localhost" // this will change in the future
+        this.connection = mysql.createConnection({
+            host: host,
+            user: dbUser,
+            password: dbPassword,
+            database: dbName
+        });
+    }
+
+    async connect(): Promise<boolean> {
+        return await new Promise((resolve) => this.connection.connect(err => {
+            if (err) {
+                return resolve(false)
+            };
+            return resolve(true)
+        }))
+    }
+
+    async getTables() {
+        try {
+            // paginated results
+            const [results, fields]: any = await new Promise((resolve, reject) => {
+                this.connection.query(
+                    "SHOW TABLES;",
+                    (err, results, fields) => {
+                        if (err) return reject(err);
+                        resolve([results, fields.map(f => {
+                            delete f._buf
+                            return f
+                        })]);
+                    }
+                );
+            });
+
+            return {
+                entries: results.map(r => {
+                    return {
+                        table_name: Object.values(r)[0]
+                    }
+                }),
+                fields
+            };
+        } catch (err) {
+            console.log("error fetching data from db: ", err)
+            return null
+        }
+    }
+
+    async getTableItems(table: string, page: number = 1, limit: number = 25): Promise<any> {
+        try {
+            const offset = (page - 1) * limit;
+
+            // get columns names to enable ordering
+            const columns: any[] = await new Promise((resolve, reject) => {
+                this.connection.query(`SHOW COLUMNS FROM \`${table}\`;`, (err, results) => {
+                    if (err) return resolve([]);
+                    resolve(results);
+                });
+            });
+
+            if (!columns || columns.length === 0) {
+                throw new Error(`No columns found for table "${table}"`);
+            }
+            const columnNames = columns.map(col => col.Field);
+            const sortColumn = columnNames.includes('id') ? 'id' : columnNames[0];
+
+            // paginated results
+            const [results, fields]: any = await new Promise((resolve, reject) => {
+                this.connection.query(
+                    `SELECT * FROM \`${table}\` ORDER BY \`${sortColumn}\` LIMIT ? OFFSET ?`,
+                    [limit, offset],
+                    (err, results, fields) => {
+                        if (err) return reject(err);
+                        resolve([results, fields.map(f => {
+                            delete f._buf
+                            return f
+                        })]);
+                    }
+                );
+            });
+
+            return {
+                sort_by: sortColumn,
+                page,
+                limit,
+                entries: results,
+                fields
+            };
+        } catch (err) {
+            console.log("error fetching data from db: ", err)
+            return null
+        }
+    }
+
+    async getItem(table: string, field: string, fieldValue: string, page: number = 1, limit: number = 25): Promise<any> {
+        try {
+            const offset = (page - 1) * limit;
+
+            // get columns names to enable ordering
+            const columns: any[] = await new Promise((resolve, reject) => {
+                this.connection.query(`SHOW COLUMNS FROM \`${table}\`;`, (err, results) => {
+                    if (err) return resolve([]);
+                    resolve(results);
+                });
+            });
+
+            if (!columns || columns.length === 0) {
+                throw new Error(`No columns found for table "${table}"`);
+            }
+            const columnNames = columns.map(col => col.Field);
+            const sortColumn = columnNames.includes('id') ? 'id' : columnNames[0];
+
+            // paginated results
+            const [results, fields]: any = await new Promise((resolve, reject) => {
+                this.connection.query(
+                    `SELECT * FROM \`${table}\` WHERE \`${field}\` = ${fieldValue} LIMIT ? OFFSET ?`,
+                    [limit, offset],
+                    (err, results, fields) => {
+                        if (err) return reject(err);
+                        resolve([results, fields.map(f => {
+                            delete f._buf
+                            return f
+                        })]);
+                    }
+                );
+            });
+
+            return {
+                sort_by: sortColumn,
+                page,
+                limit,
+                entries: results,
+                fields
+            };
+        } catch (err) {
+            console.log("error fetching data from db: ", err)
+            return null
+        }
+    }
+
+    async close() {
+        await this.connection.end();
+    }
+}
